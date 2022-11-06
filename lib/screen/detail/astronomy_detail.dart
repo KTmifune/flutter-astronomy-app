e472907c112e33c8fa131a7a6aab3a4ad46db4c5
Takuma Kato <k.t.mifune@gmail.com>
2022-11-06 19:03:27 +0900

    remove unnecessary import and add go_router package

--- lib/screen/detail/astronomy_detail.dart
+++ lib/screen/detail/astronomy_detail.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:space_iamge_app/data/model/astronomy_data.dart';
 import 'package:space_iamge_app/data/repository.dart';
